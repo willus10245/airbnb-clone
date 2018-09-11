acc47bf03acc190e7b8bd5aea0fa5fd943876f2a
Scott Wiggins <dscottwiggins@gmail.com>
2018-09-11 16:35:17 -0400

    creating a typeorm entity

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,18 +1,9 @@
 import "reflect-metadata";
+import { importSchema } from "graphql-import";
+import { GraphQLServer } from "graphql-yoga";
+import { resolvers } from "./resolvers";
 
-import { GraphQLServer } from 'graphql-yoga';
+const typeDefs = importSchema("schema.graphql");
 
-const typeDefs = `
-  type Query {
-    hello(name: String!): String!
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
-  },
-}
-
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000'))
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
